cb8c96a732a7c20aa2db5384b6aa27e74500a7ad
truongnguyen131 <69027582+truongnguyen131@users.noreply.github.com>
2023-04-27 19:12:19 +0700

    tao sửa xong rồi đó

--- page/quanly-admin.php
+++ page/quanly-admin.php
@@ -13,11 +13,7 @@ include_once('database_connection.php'); ?>
     <!-- bootstrap css -->
     <link rel="stylesheet" href="../css/bootstrap.min.css" />
     <!-- style css -->
-    <link rel="stylesheet" href="../css/style.css" />
-    <link rel="stylesheet" href="../css/responsive.css" />
     <link rel="stylesheet" href="../css/quanly-admin.css" />
-    <link rel="stylesheet" href="../css/stars.css" />
-    <link rel="stylesheet" href="../css/click_slider.css" />
     <link rel="stylesheet prefetch" href="https://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" />
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
