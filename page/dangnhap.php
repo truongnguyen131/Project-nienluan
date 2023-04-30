f3cbca11915ff4d6c6d98672804b9762d325de6d
truongnguyen131 <69027582+truongnguyen131@users.noreply.github.com>
2023-04-30 09:28:32 +0700

    update and create Đăng ký và đăng nhập
    sửa lại giao diện đăng nhập và đăng ký
    tạo file js cho đăng ký và đăng nhập
    xóa 1 số hình trong folder images

--- page/dangnhap.php
+++ page/dangnhap.php
@@ -12,16 +12,6 @@ include_once('database_connection.php'); ?>
 
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-    <style>
-        .is-invalid {
-            border: 1px solid red;
-            vertical-align: center;
-        }
-
-        .loi {
-            color: red;
-        }
-    </style>
     <title>Document</title>
 </head>
 
@@ -45,7 +35,6 @@ include_once('database_connection.php'); ?>
         echo ' <script>window.location="http://localhost/Project-nienluan/page/chitietsanpham.php?idsp=' . $idsp . '"</script>';
     }
     ?>
-
     <form action="" method="post">
         <div class="login-form">
             <div class="login-title">
@@ -78,80 +67,13 @@ include_once('database_connection.php'); ?>
             </div>
         </div>
     </form>
+
 </body>
 <!-- Javascript files-->
 <script src="../js/jquery.min.js"></script>
 <script src="../js/popper.min.js"></script>
 <script src="../js/bootstrap.bundle.min.js"></script>
 <script src="../js/jquery-3.0.0.min.js"></script>
-<script>
-
-    function Huy() {
-        document.getElementById('tk').value = "";
-        document.getElementById('mk').value = "";
-    }
-
-    function kiemtraloi() {
-        var check = 0
-        var tk = $('#tk').val()
-        var mk = $('#mk').val()
-        var ntk = "unchecked"
-        if (mk.length == 0) {
-            check -= 1
-            $('#mk').addClass('is-invalid');
-            $('#loimk').html("Mật khẩu không được rỗng")
-        } else {
-            $('#mk').removeClass('is-invalid')
-            $('#loimk').html("")
-            check += 1
-        }
-
-        if (tk.length == 0) {
-            check -= 1
-            $('#tk').addClass('is-invalid');
-            $('#loitk').html("Tài khoản không được rỗng")
-        } else {
-            $('#tk').removeClass('is-invalid')
-            $('#loitk').html("")
-            check += 1
-        }
-
-        if (document.getElementById('ntk').checked) {
-            ntk = "checked";
-        }
-
-
-
-        if (check == 2) {
-            $.post('xulydangnhap.php', {
-                ntk1: ntk,
-                taikhoan1: tk,
-                matkhau1: mk
-            }, function (data) {
-                $('body').html(data);
-            })
-
-        }
-
-    }
-</script>
-
-
-<script>
-    const input = document.querySelector(".input-pass");
-    const openeye = document.querySelector(".eye-open");
-    const closeeye = document.querySelector(".eye-close");
-
-    closeeye.addEventListener("click", function () {
-        closeeye.classList.add("hidden");
-        openeye.classList.remove("hidden");
-        input.setAttribute("type", "text");
-    });
-    openeye.addEventListener("click", function () {
-        openeye.classList.add("hidden");
-        closeeye.classList.remove("hidden");
-        input.setAttribute("type", "password");
-    });
-</script>
+<script src="../js/dangnhap.js"></script>
 
 </html>
