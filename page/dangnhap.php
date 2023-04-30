5c8307426c81642bc4b569104f2882b9b4ebca9f
Student1220517 <79580131+Student1220517@users.noreply.github.com>
2023-04-30 12:20:47 +0700

    update dang nhap

--- page/dangnhap.php
+++ page/dangnhap.php
@@ -13,14 +13,14 @@ include_once('database_connection.php'); ?>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     <style>
-        .is-invalid {
-            border: 1px solid red;
-            vertical-align: center;
-        }
+    .is-invalid {
+        border: 1px solid red;
+        vertical-align: center;
+    }
 
-        .loi {
-            color: red;
-        }
+    .loi {
+        color: red;
+    }
     </style>
     <title>Document</title>
 </head>
@@ -29,7 +29,12 @@ include_once('database_connection.php'); ?>
     <?php
     if (isset($_SESSION['dangnhapthanhcong']) && $_SESSION['dangnhapthanhcong'] == "khongthanhcong") {
         unset($_SESSION['dangkythanhcong']);
-        echo "<script>alert('Đăng nhập không thành công!!')</script>";
+        echo "<script>
+        $('#mk').addClass('is-invalid');
+            $('#loimk').html('Mật khẩu không hợp lệ');
+            $('#tk').addClass('is-invalid');
+            $('#loitk').html('Tài khoản không hợp lệ');
+        </script>";
     }
     if (isset($_SESSION['dangnhapthanhcong']) && $_SESSION['dangnhapthanhcong'] == "thanhcong") {
         unset($_SESSION['dangkythanhcong']);
@@ -85,73 +90,72 @@ include_once('database_connection.php'); ?>
 <script src="../js/bootstrap.bundle.min.js"></script>
 <script src="../js/jquery-3.0.0.min.js"></script>
 <script>
+function Huy() {
+    document.getElementById('tk').value = "";
+    document.getElementById('mk').value = "";
+}
 
-    function Huy() {
-        document.getElementById('tk').value = "";
-        document.getElementById('mk').value = "";
+function kiemtraloi() {
+    var check = 0
+    var tk = $('#tk').val()
+    var mk = $('#mk').val()
+    var ntk = "unchecked"
+    if (mk.length == 0) {
+        check -= 1
+        $('#mk').addClass('is-invalid');
+        $('#loimk').html("Mật khẩu không được rỗng")
+    } else {
+        $('#mk').removeClass('is-invalid')
+        $('#loimk').html("")
+        check += 1
     }
 
-    function kiemtraloi() {
-        var check = 0
-        var tk = $('#tk').val()
-        var mk = $('#mk').val()
-        var ntk = "unchecked"
-        if (mk.length == 0) {
-            check -= 1
-            $('#mk').addClass('is-invalid');
-            $('#loimk').html("Mật khẩu không được rỗng")
-        } else {
-            $('#mk').removeClass('is-invalid')
-            $('#loimk').html("")
-            check += 1
-        }
+    if (tk.length == 0) {
+        check -= 1
+        $('#tk').addClass('is-invalid');
+        $('#loitk').html("Tài khoản không được rỗng")
+    } else {
+        $('#tk').removeClass('is-invalid')
+        $('#loitk').html("")
+        check += 1
+    }
 
-        if (tk.length == 0) {
-            check -= 1
-            $('#tk').addClass('is-invalid');
-            $('#loitk').html("Tài khoản không được rỗng")
-        } else {
-            $('#tk').removeClass('is-invalid')
-            $('#loitk').html("")
-            check += 1
-        }
-
-        if (document.getElementById('ntk').checked) {
-            ntk = "checked";
-        }
+    if (document.getElementById('ntk').checked) {
+        ntk = "checked";
+    }
 
 
 
-        if (check == 2) {
-            $.post('xulydangnhap.php', {
-                ntk1: ntk,
-                taikhoan1: tk,
-                matkhau1: mk
-            }, function (data) {
-                $('body').html(data);
-            })
-
-        }
+    if (check == 2) {
+        $.post('xulydangnhap.php', {
+            ntk1: ntk,
+            taikhoan1: tk,
+            matkhau1: mk
+        }, function(data) {
+            $('body').html(data);
+        })
 
     }
+
+}
 </script>
 
 
 <script>
-    const input = document.querySelector(".input-pass");
-    const openeye = document.querySelector(".eye-open");
-    const closeeye = document.querySelector(".eye-close");
+const input = document.querySelector(".input-pass");
+const openeye = document.querySelector(".eye-open");
+const closeeye = document.querySelector(".eye-close");
 
-    closeeye.addEventListener("click", function () {
-        closeeye.classList.add("hidden");
-        openeye.classList.remove("hidden");
-        input.setAttribute("type", "text");
-    });
-    openeye.addEventListener("click", function () {
-        openeye.classList.add("hidden");
-        closeeye.classList.remove("hidden");
-        input.setAttribute("type", "password");
-    });
+closeeye.addEventListener("click", function() {
+    closeeye.classList.add("hidden");
+    openeye.classList.remove("hidden");
+    input.setAttribute("type", "text");
+});
+openeye.addEventListener("click", function() {
+    openeye.classList.add("hidden");
+    closeeye.classList.remove("hidden");
+    input.setAttribute("type", "password");
+});
 </script>
 
 </html>
