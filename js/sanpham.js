4aacc526492257d918a92715c3b37c5d1e606b11
Student1220517 <79580131+Student1220517@users.noreply.github.com>
2023-05-07 21:16:27 +0700

    .

--- js/sanpham.js
+++ js/sanpham.js
@@ -5,6 +5,7 @@ let progress = document.querySelector('.pro');
 let priceMax = rangeInput[0].max;
 let priceGap = 1000;
 
+
 rangeInput.forEach(input => {
     input.addEventListener('input', (event) => {
         let minVal = parseInt(rangeInput[0].value);
@@ -48,11 +49,7 @@ function opennsx() {
     modal.style.display = "none";
 }
 
-function returnmodal() {
-    modal.style.display = "block";
-    nsx.style.display = "none";
-    theloai.style.display = "none";
-}
+
 
 function openmodal() {
     modal.style.display = "block";
@@ -67,75 +64,3 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
-
-// pagination
-$(function() {
-    var pg_links = $(".pg-link"),
-        h_elm = $("#m-pg-link"),
-        curr = 5,
-        _this,
-        _class,
-        _id,
-        __this,
-        _num,
-        i = 0;
-
-    function changeNumbers(flag) {
-        if ((curr > 2 && flag == 0) || flag) {
-            if (curr == 1) {
-                curr = 2;
-                h_elm.removeClass("left");
-                return;
-            }
-
-            $(pg_links).each(function() {
-                __this = $(this).find("span");
-                _num = parseInt(__this.text());
-
-                if (flag) _num += 1;
-                else _num -= 1;
-
-                ++i;
-
-                pg_links.addClass("s-hide");
-
-                __this.text(_num);
-
-                setTimeout(function() {
-                    pg_links.removeClass("s-hide");
-                }, 150);
-
-                if (i == 2) curr = _num;
-            });
-
-            i = 0;
-        } else {
-            if (curr == 2 && flag == 0) {
-                curr = 1;
-                h_elm.addClass("left");
-            }
-        }
-    }
-
-    function changeLinks() {
-        _this = $(this);
-        _class = _this.attr("class").trim().toLowerCase();
-
-        if (_class == "arrow") {
-            _id = _this.attr("id").trim().toLowerCase();
-
-            if (_id == "l-arrow") changeNumbers(0);
-            else changeNumbers(1);
-        } else {
-            if (parseInt(_this.text().trim()) < curr) changeNumbers(0);
-            else changeNumbers(1);
-        }
-    }
-
-    $(".pg-link,.arrow").on("click", changeLinks);
-});
-
-
-
-
-// Xử lí filter
