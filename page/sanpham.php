9fcd20daf0a04c388b8c4891919dd4cd9f5c365b
Student1220517 <79580131+Student1220517@users.noreply.github.com>
2023-05-09 12:11:43 +0700

    .

--- page/sanpham.php
+++ page/sanpham.php
@@ -278,7 +278,7 @@ if (!function_exists('currency_format')) {
                                             </div>
                                             <!-- button thêm vào giỏ hàng -->
                                             <div class="card-button">
-                                                <button style="display: block;"
+                                                <button 
                                                     onclick="themsanphamindex(<?php echo $row['sp_id']; ?>)">
                                                     <ion-icon name="cart-outline"></ion-icon>
                                                 </button>
